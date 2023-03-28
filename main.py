ad617dfa44e77f188b850dbc62047331d7da8c84
Philipp <kinzarra@yandex.ru>
2023-03-28 19:04:54 +0300

    Add webhooks gunicorn and flask

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 import telebot
 
-from flask import Flask
-from services.config import BOT_TOKEN
+from flask import Flask, request
+from services.config import BOT_TOKEN, APP_URL
 from services.sugar import get_answer
 from services.messages import BOT_COMMANDS
 
+WEBHOOK_URL = f"{APP_URL}/{BOT_TOKEN}"
+
 bot = telebot.TeleBot(BOT_TOKEN)
+bot.remove_webhook()
+bot.set_webhook(url=WEBHOOK_URL)
 
 app = Flask(__name__)
 
 
-@bot.message_handler(func=lambda message: True)
-# @app.route('/' + BOT_TOKEN, methods=['GET', 'POST'])
-def echo_message(message):
-    try:
-        answer = BOT_COMMANDS[message.text]
-        bot.send_message(message.chat.id, answer)
-    except KeyError:
-        answer = get_answer(message.text)
-        bot.reply_to(message, answer)
+@app.route('/' + BOT_TOKEN, methods=['POST'])
+def receive_message():
+    update = telebot.types.Update.de_json(request.get_json(force=True))
+    if update.message:
+        try:
+            message = update.message
+            answer = BOT_COMMANDS[message.text]
+            bot.send_message(message.chat.id, answer)
+        except KeyError:
+            answer = get_answer(message.text)
+            bot.reply_to(message, answer)
+
+    return 'ok', 200
 
 
-if __name__ == '__main__':
-    bot.polling()
-    # app.run()
+# if __name__ == '__main__':
+#     # bot.polling()
+#     bot.remove_webhook()
+#     bot.set_webhook(url=WEBHOOK_URL)
+#     app.run(host='0.0.0.0', port=8080, debug=False, threaded=True)
--- services/config.py
+++ services/config.py
@@ -1,7 +1,13 @@
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Telegram Bot
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 # SUGAR AI API
 SUGAR_AI_API = os.environ.get("SUGAR_AI_API")
+
+# APP
+APP_URL = os.environ.get("APP_URL")
