e338a3a4deb18e78cee2bb50624ae2456de1478b
Philipp <kinzarra@yandex.ru>
2023-05-18 23:42:55 +0300

    Change lang to English

--- services/messages.py
+++ services/messages.py
@@ -1,25 +1,27 @@
-BOT_VERSION = "1.0.3"
-BOT_UPDATED = "30.03.2023"
+BOT_VERSION = "1.0.5"
+BOT_UPDATED = "17.05.2023"
 BOT_COMMANDS = {
-    "start": "Привет, я ИИ ассистент Sugar Pulse, я знаю абсолютно все про сахарный диабет.\n"
-              "Какой у вас вопрос?",
-    "help": "Команды:\n"
-             "/start - Запустить бота\n"
-            "/ask - Спросить бота \n"
-             "/help - Список доступных команд\n"
-             "/insert - Записать новый факт\n"
-             "/about - Информация о боте",
-    "about": f"Версия бота {BOT_VERSION} от {BOT_UPDATED} \n"
-              f"ИИ ассистент Sugar Pulse",
-    "insert": "Напишите факт, который мне нужно запомнить \n"
-               "Например: В 2030 году британские ученые открыли сахарный диабет десятого типа",
-    "ask": "Какой у вас вопрос?",
-    "cancel": "Отменить",
-    "cancel_action": "Отменено"
+    "start": "Hello! I'm Sugar Pulse, your AI assistant, and "
+             "I'm here to help you with any questions you have about diabetes.\n"
+             "Feel free to ask me anything you'd like to know. "
+             "I'm well-versed in all aspects of this topic and eager to assist you!",
+    "help": "Commands:\n"
+            "/start - Run bot\n"
+            "/ask - Ask bot \n"
+            "/help - List of available commands \n"
+            "/insert - Insert new fact about diabetes \n"
+            "/about - Information about bot",
+    "about": f"Version of the bot {BOT_VERSION} from {BOT_UPDATED} \n"
+             f"AI assistant Sugar Pulse",
+    "insert": "Write a fact that I have to learn \n"
+              "For example: In 2030 British scientists discovered the tenth type of diabetes related to sugar.",
+    "ask": "What question do you want to ask me?",
+    "cancel": "Cancel",
+    "cancel_action": "Cancelled"
 }
 
 BOT_MESSAGES = {
-    "timeout": "С моей базой знаний что-то случилось...",
-    "insert_ok": "Я успешно запомнил этот интересный факт...",
-    "insert_error": "Что-то случилось и мне не удалось запомнить этот факт..."
+    "timeout": "Something wrong with my Data Knowledge...",
+    "insert_ok": "That's wonderful to hear! I learned this fact...",
+    "insert_error": "Something happened and I did not learn this fact..."
 }
